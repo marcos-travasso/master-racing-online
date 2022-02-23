b0ae3d4ac5868cf4d0e828eb1933f5df0e56f26a
Marcos Travasso <marcos.travasso42@gmail.com>
2022-02-23 16:34:01 -0300

    Initial commit

---
+++ src/main/java/online/masterracing/MasterRacingOnlineApplication.java
@@ -0,0 +1,13 @@
+package online.masterracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterRacingOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterRacingOnlineApplication.class, args);
+    }
+
+}
